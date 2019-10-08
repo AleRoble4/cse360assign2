086b056d7d1b5754e9c77741e972aa7ab54f4e65
Alejandro <aroble24@asu.edu>
2019-10-08 12:58:11 -0700

    Initial Version of Project

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360assign2;
+
+/**
+ * Represents a calculator and can do multiple functions
+ * such as add and subtract while displaying a total
+ */
+public class AddingMachine {
+	/**
+	 * Result of the operations used.
+	 */
+	private int total;
+	
+	/*
+	 * 
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Method to get the total for an operation.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Method used to add to a number.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Method used to subtract from a number.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Method to keep history of transaction and display. 
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Method to clear all entries into program.
+	 */
+	public void clear() {
+	
+	}
+}
