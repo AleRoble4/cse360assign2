81e12947d34cbe09a58d4fc3d050b43eca68a30a
Alejandro <vicedynasty7@gmail.com>
2019-10-08 14:55:56 -0700

    Second Version of AddingMachine. Added Test.Java file for testing.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,52 +1,64 @@
 package cse360assign2;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 
 /**
  * Represents a calculator and can do multiple functions
  * such as add and subtract while displaying a total
  */
 public class AddingMachine {
-	/**
-	 * Result of the operations used.
-	 */
+	
+	Scanner scan = new Scanner(System.in);
+	
 	private int total;
+	private ArrayList<String> history = new ArrayList<String>();
 	
 	/*
-	 * 
+	 * Constructor
 	 */
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		
 	}
 	
 	/**
 	 * Method to get the total for an operation.
 	 */
 	public int getTotal () {
-		return 0;
+		System.out.println("Total: " + total);
+		return total;
 	}
 	
 	/**
 	 * Method used to add to a number.
 	 */
 	public void add (int value) {
-		
+		total = total + value;
+		history.add("+ " + value);
 	}
 	
 	/**
 	 * Method used to subtract from a number.
 	 */
 	public void subtract (int value) {
-		
+		total = total - value;
+		history.add("- " + value);
 	}
 	
 	/**
 	 * Method to keep history of transaction and display. 
 	 */
 	public String toString () {
+		System.out.print("0 ");
+		for(String name:history)
+		{
+			System.out.print(name + " ");
+		}
 		return "";
 	}
 
 	/**
-	 * Method to clear all entries into program.
+	 * N/A
 	 */
 	public void clear() {
 	
---
+++ cse360assign2/src/cse360assign2/Test.java
@@ -0,0 +1,21 @@
+package cse360assign2;
+import java.util.Scanner;
+
+public class Test {
+	
+	Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add(4);
+		myCalculator.getTotal();
+		myCalculator.subtract(2);
+		myCalculator.getTotal();
+		myCalculator.add(5);
+		myCalculator.getTotal();
+		myCalculator.toString();
+
+	}
+
+}
